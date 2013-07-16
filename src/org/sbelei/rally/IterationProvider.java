345899d25de4d1e48af42807250183ee2d6029be
crc83 <sergiy.beley@gmail.com>
2013-07-16 09:13:34 +0300

    Iteration processor enhanced

--- src/org/sbelei/rally/IterationProvider.java
+++ src/org/sbelei/rally/IterationProvider.java
@@ -1,6 +1,12 @@
 package org.sbelei.rally;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+
+import org.sbelei.rally.domain.BasicEntity;
+import org.sbelei.rally.jsonprocessor.BasicEntityProcessor;
 
 import com.rallydev.rest.RallyRestApi;
 import com.rallydev.rest.request.QueryRequest;
@@ -12,25 +18,9 @@ import static org.sbelei.rally.QueryHelper.*;
 public class IterationProvider {
 	
 	String workspaceId = "41593629";
+	String projectId = "9216950819";
 	RallyRestApi resApi;
 	
-	public void fetchData(){
-		QueryRequest request = new QueryRequest("iteration");
-		request.setWorkspace(workspaceId);
-		QueryFilter projectFilter = byProjectId("9216950819");
-		QueryFilter dateFilter = isDateInIteration(new Date());
-		request.setQueryFilter(projectFilter.and(dateFilter));
-
-//			.and(new QueryFilter("State", "<", "Fixed"))		
-//			.and(new QueryFilter("Iteration.ObjectID", "=", "11597902889")
-//			
-//			))
-		
-		QueryResponse queryResponse = resApi.query(request);
-//		EntityProcessor.saveResponceToFile("${home}/iteration-responce.json",queryResponse.getResults().toString());
-		List<Project> projects= EntityProcessor.fetchProjects(queryResponse.getResults());
-	}
-
 	private QueryFilter isDateInIteration(Date date) {
 		QueryFilter startDateFilter = new QueryFilter("StartDate","<=",queryDate(date));
 		QueryFilter endDateFilter = new QueryFilter("EndDate",">",queryDate(date));
@@ -38,11 +28,36 @@ public class IterationProvider {
 	}
 	
 	public List<BasicEntity> getIterations(){
+		return fetch(null);
+	}
+
+	private List<BasicEntity> fetch(QueryFilter additionalFilter) {
+		QueryRequest request = new QueryRequest("iteration");
+		request.setWorkspace(workspaceId);
+		QueryFilter projectFilter = byProjectId(projectId);
+
+		if (additionalFilter == null){
+			request.setQueryFilter(projectFilter);
+		} else {
+			request.setQueryFilter(projectFilter.and(additionalFilter));
+		}
 		
+		QueryResponse responce;
+		List<BasicEntity> result;
+		try {
+			responce = resApi.query(request);
+			result = new BasicEntityProcessor().fetchBasicEntities(responce.getResults());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			result = new ArrayList<>();
+		}
+		return result;
 	}
 	
 	public BasicEntity getCurrentIteration(){
-		
+		//TODO SB : Fix it
+		return fetch(isDateInIteration(new Date())).get(0);
 	}
 
 }
